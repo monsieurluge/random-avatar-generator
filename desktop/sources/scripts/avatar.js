561840183d2646c89a4592b111a4bbe33cea878a
monsieurluge <monsieurluge@gmail.com>
2020-05-13 15:16:25 +0200

    set a random avatar background color

--- desktop/sources/scripts/avatar.js
+++ desktop/sources/scripts/avatar.js
@@ -4,8 +4,9 @@ function Avatar() {
   this.draw = function(context) {
     const width = context.canvas.width
     const height = context.canvas.height
+    const color = randomColor()
     context.clearRect(0, 0, width, height)
-    context.fillStyle = 'crimson'
+    context.fillStyle = `rgb(${color.red}, ${color.green}, ${color.blue})`
     context.fillRect(4, 4, width - 8, height - 8)
   }
 }
