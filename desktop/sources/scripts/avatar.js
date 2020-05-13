b3db0a10678d25d40de15a8f0587e94d3c710687
monsieurluge <monsieurluge@gmail.com>
2020-05-13 15:54:46 +0200

    draw the avatar background color, centered

--- desktop/sources/scripts/avatar.js
+++ desktop/sources/scripts/avatar.js
@@ -1,12 +1,18 @@
 'use strict'
 
 function Avatar() {
+
+  this.baseColor = randomRgbColor()
+
   this.draw = function(context) {
-    const width = context.canvas.width
     const height = context.canvas.height
-    const color = randomRgbColor()
+    const width = context.canvas.width
+    const size = Math.min(height, width)
+    const padding = (height > width)
+      ? { top: (height - width) / 2, left: 0 }
+      : { top: 0, left: (width - height) / 2 }
     context.clearRect(0, 0, width, height)
-    context.fillStyle = cssRgbColor(color)
-    context.fillRect(4, 4, width - 8, height - 8)
+    context.fillStyle = cssRgbColor(this.baseColor)
+    context.fillRect(0 + padding.left, 0 + padding.top, size, size)
   }
 }
