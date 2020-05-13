f9eeddc846c72f39ec79acbb469708714ca2f7ea
monsieurluge <monsieurluge@gmail.com>
2020-05-13 23:32:34 +0200

    increase the next color range

--- desktop/sources/scripts/lib/rgb-color.js
+++ desktop/sources/scripts/lib/rgb-color.js
@@ -2,7 +2,7 @@
 
 const cssRgbColor = color => `rgb(${color.red}, ${color.green}, ${color.blue})`
 
-const nextRgbColor = color => ({ red: color.red - 5, green: color.green + 5, blue: color.blue + 5 })
+const nextRgbColor = color => ({ red: color.red - 10, green: color.green + 10, blue: color.blue + 10 })
 
 const randomRgbValue = randomValue(256)
 
