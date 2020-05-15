0bc93f4aa2046dc6fba7d8d4f26de0849a3ba691
monsieurluge <monsieurluge@gmail.com>
2020-05-15 15:11:09 +0200

    remove unused settings

--- desktop/sources/scripts/client.js
+++ desktop/sources/scripts/client.js
@@ -1,19 +1,10 @@
 'use strict'
 
 function Client() {
-  const padding = { height: 120, width: 120 }
-
   this.install = function(host) {
-    this.avatar = new Avatar({ seed: 666})
+    this.avatar = new Avatar()
 
     this.avatar.install(host)
-  }
-
-  this.paddedSize = function() {
-    return {
-      height: window.innerHeight - padding.height,
-      width: window.innerWidth - padding.width
-    }
   }
 
   this.start = function() {
