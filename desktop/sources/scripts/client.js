835021dfe1fed63c99c8980a504b7daefea9b3cb
monsieurluge <monsieurluge@gmail.com>
2020-05-17 23:08:43 +0200

    init the interface object

--- desktop/sources/scripts/client.js
+++ desktop/sources/scripts/client.js
@@ -3,11 +3,14 @@
 function Client() {
   this.install = function(host) {
     this.avatar = new Avatar()
+    this.interface = new Interface()
 
     this.avatar.install(host)
+    this.interface.install(host)
   }
 
   this.start = function() {
     this.avatar.draw()
+    this.interface.start()
   }
 }
---
+++ desktop/sources/scripts/interface.js
@@ -0,0 +1,31 @@
+'use strict'
+
+function Interface() {
+  this.install = function (host) {
+    this.tools = document.createElement('div')
+    this.tools.id = 'tools-bar'
+
+    host.appendChild(this.tools)
+  }
+
+  this.start = function () {
+    const tools = [
+      { name: 'grain', icon: 'M60 120 H240 M60 180 H240 M120 60 V240 M180 60 V240' },
+    ]
+
+    let htmlContent = '<div class="tools">'
+
+    tools.forEach(tool => {
+      htmlContent += `
+      <svg title="${name}" viewbox="0 0 300 300" class="icon">
+        <path class="icon_path" d="${tool.icon}"/>
+      </svg>`
+    })
+
+    htmlContent += '</div>'
+
+    console.log(htmlContent)
+
+    this.tools.innerHTML = htmlContent
+  }
+}
